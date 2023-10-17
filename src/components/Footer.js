fe2f14649f56cb64607b547dcc3844046c741d9a
HARSH SHUKLA <HARSHSHUKLA2022@VITBHOPAL.AC.IN>
2023-10-17 23:37:32 +0530

    updated css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo2.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
@@ -16,14 +15,20 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a
+                href="https://www.linkedin.com/in/harsh-shukla-1inkdein/"
+                target="_blank"
+              >
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="https://instagram.com/haarrrssshhh_" target="_blank">
+                <img src={navIcon3} alt="" />
+              </a>
             </div>
             <p>Copyright @ Harsh Shukla All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
     </footer>
-  )
+  );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo2.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
@@ -44,22 +43,45 @@ export const NavBar = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>About Us</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className={
+                  activeLink === "projects"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("projects")}
+              >
+                About Us
+              </Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/harsh-shukla-1inkdein/" target="_blank">
+                  <img src={navIcon1} alt="" />
+                </a>
+                <a href="https://instagram.com/haarrrssshhh_" target="_blank">
+                  <img src={navIcon3} alt="" />
+                </a>
               </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+              <HashLink to="#connect">
+                <button className="vvd">
+                  <span>Let’s Connect</span>
+                </button>
               </HashLink>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </Router>
-  )
+  );
 }
